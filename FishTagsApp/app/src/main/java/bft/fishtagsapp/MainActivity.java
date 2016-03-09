a70106511339827fc9deb089061c4fd559c9218e
yycho0108 <jchocholate@gmail.com>
2016-03-09 03:34:52 -0500

    Imported Client and Completed Testing

--- FishTagsApp/app/src/main/java/bft/fishtagsapp/MainActivity.java
+++ FishTagsApp/app/src/main/java/bft/fishtagsapp/MainActivity.java
@@ -1,5 +1,7 @@
 package bft.fishtagsapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +10,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import bft.fishtagsapp.bft.fishtagsapp.Client.Uploader;
 
 public class MainActivity extends AppCompatActivity {
+
+    Uploader uploader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,18 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        /* Client Code Start */
+        String url = "http://192.168.16.67:8000/";
+        uploader = new Uploader(this,url);
+        Button testBtn = (Button) findViewById(R.id.testBtn);
+        testBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                test();
+            }
+        });
+        /* Client Code End */
     }
 
     @Override
@@ -49,4 +69,41 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    /* TEST CODE FOR CLIENT START */
+
+    private static final int GET_TEST_FILE_CODE = 100;
+
+    public void test() {
+        /* Testing Purposes*/
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        try {
+            startActivityForResult(
+                    Intent.createChooser(intent, "Select a File to Upload"), GET_TEST_FILE_CODE);
+        } catch (android.content.ActivityNotFoundException ex) {
+            // Potentially direct the user to the Market with a Dialog
+            Toast.makeText(this, "Please install a File Manager.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case GET_TEST_FILE_CODE:
+                if (resultCode == RESULT_OK) {
+                    try {
+                        Uri uri = data.getData();
+                        uploader.send(uri, "PARAM1", "PARAM2");
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
+                }
+                break;
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    /* TEST CODE FOR CLIENT END */
 }
