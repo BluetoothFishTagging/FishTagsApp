7be20923e64859c57a8d83b675f453aaa6e81718
yycho0108 <jchocholate@gmail.com>
2016-03-09 02:35:44 -0500

    First Commit; Imported & Tested GPS Branch with Invocation Example Code

---
+++ FishTagsApp/app/src/main/java/bft/fishtagsapp/GPS/GPS.java
@@ -0,0 +1,88 @@
+package bft.fishtagsapp.GPS;
+
+import android.content.Context;
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by jamiecho on 3/9/16.
+ */
+public class GPS implements LocationListener {
+
+    private LocationManager locationManager;
+    private Location location;
+    private String provider;
+    private Context context;
+
+
+    public GPS(Context context){
+        this.context = context;
+    }
+
+    public void enableGPS(){
+        locationManager = (LocationManager) context.getSystemService(context.LOCATION_SERVICE);
+        boolean enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+// check if enabled and if not send user to the GSP settings
+// Better solution would be to display a dialog and suggesting to
+// go to the settings
+        if (!enabled) {
+            Log.i("ENABLED", "FALSE");
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            context.startActivity(intent);
+        }
+        Log.i("ENABLED","TRUE");
+        //hopefully user will enable GPS here
+    }
+    public Location getGPS() {
+        enableGPS(); //in case not enabled
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, true);
+        Log.i("PROVIDER : ", provider);
+        try {
+            //to be safe
+            locationManager.requestLocationUpdates(provider, 0, 0, this);
+            location = locationManager.getLastKnownLocation(provider);
+
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+        // Initialize the location fields
+        Log.i("LOCATION : ", String.valueOf(location));
+
+        if (location != null) {
+            System.out.println("Provider " + provider + " has been selected.");
+            onLocationChanged(location);
+
+        }
+        return location;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        this.location = location;
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        //nothing yet
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(context, "Enabled new provider " + provider,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(context, "Disabled provider " + provider,
+                Toast.LENGTH_SHORT).show();
+    }
+}
--- FishTagsApp/app/src/main/java/bft/fishtagsapp/MainActivity.java
+++ FishTagsApp/app/src/main/java/bft/fishtagsapp/MainActivity.java
@@ -1,5 +1,6 @@
 package bft.fishtagsapp;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,11 +9,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import bft.fishtagsapp.GPS.GPS;
 
 public class MainActivity extends AppCompatActivity {
+    private GPS gps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -26,6 +33,26 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+
+        /* How To Invoke GPS */
+        gps = new GPS(this);
+        Button GPSBtn = (Button) findViewById(R.id.GPS_GPSBtn);
+        GPSBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Location location = gps.getGPS();
+                if(location != null){
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    String s = String.format("LAT:%f,LONG:%f",latitude,longitude);
+                    Toast.makeText(getApplicationContext(),s,Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        /* GPS End */
+
     }
 
     @Override
