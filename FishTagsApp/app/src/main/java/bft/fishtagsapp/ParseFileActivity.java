552e72e706bcfcb7a299fb7dc24feea902ec163f
ksoltan <kattsoltan@gmail.com>
2016-03-30 19:49:34 -0400

    Tested reading file from Downloads on external storage of Samsung. Next step is to auto fill in a layout with different fields

--- FishTagsApp/app/src/main/java/bft/fishtagsapp/ParseFileActivity.java
+++ FishTagsApp/app/src/main/java/bft/fishtagsapp/ParseFileActivity.java
@@ -1,5 +1,4 @@
 package bft.fishtagsapp;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
@@ -7,7 +6,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -31,19 +33,12 @@ public class ParseFileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_parse_file);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Intent intent = getIntent();
-        // Do stuff
+        TextView test = (TextView)findViewById(R.id.testText);
+        //test.setText("HELLO WORLD");
+        test.setText(getEntries().toString());
     }
 
     /**
@@ -52,7 +47,8 @@ public class ParseFileActivity extends AppCompatActivity {
      * External storage currently (sdcard)
      */
     private File getStoragePath(){
-        return Environment.getExternalStorageDirectory();
+        //return Environment.getExternalStorageDirectory();
+        return Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
     }
 
     /**
@@ -61,7 +57,7 @@ public class ParseFileActivity extends AppCompatActivity {
      * Currently hardcoded to message transmitted on 3/8/2016
      */
     private String getFileName(){
-        return "2016_03_08_22_08_1157809.txt";
+        return "2016_03_08_22_08_1157890.txt";
     }
 
     protected HashMap<String, String> getEntries(){
@@ -76,12 +72,18 @@ public class ParseFileActivity extends AppCompatActivity {
                 if(numLine > 3){
                     break;
                 }
+                Log.i("Reading", line);
                 String[] l = line.split(":");
                 entries.put(l[0], l[1]);
+
+                TextView test = (TextView)findViewById(R.id.testText);
+                //test.setText("Yay");
             }
         }
         catch(IOException e){
             // Do some error handling
+            TextView test = (TextView)findViewById(R.id.testText);
+            test.setText("Error occurred");
         }
 
         return entries;
