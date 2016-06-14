a75f08f7c7ac1ca031de0b6b981aa647fbc29b1f
Katya Soltan <kattsoltan@gmail.com>
2016-06-14 12:11:31 -0400

    Added splash screen that is up only as long as the app needs to load

---
+++ FishTagsApp/app/src/main/java/bft/fishtagsapp/Splash.java
@@ -0,0 +1,18 @@
+package bft.fishtagsapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.Handler;
+
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
