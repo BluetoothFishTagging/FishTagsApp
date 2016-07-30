f2bed5c2c64cab70104389675539fa40590ac7ff
yycho0108 <jchocholate@gmail.com>
2016-07-30 18:34:50 -0400

    fixed url

--- FishTagsApp/app/src/main/java/bft/fishtagsapp/Constants.java
+++ FishTagsApp/app/src/main/java/bft/fishtagsapp/Constants.java
@@ -23,5 +23,5 @@ public final class Constants {
     public static final String ACTION_UPLOAD = "Upload";
     public static final String BLUETOOTH_DIR = "bluetoothDir";
     public static final String DEFAULT_STORE_SUBDIR = "/bft.fishtagsapp";
-    public static final String DATABASE_URL = "https://hitag-database.herokuapp.com/";
+    public static final String DATABASE_URL = "https://hitag-database.herokuapp.com/upload";
 }
